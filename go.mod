38ce5355b896e04c76737c880ffa727e0fa825c8
ChevalierSoft <ibanarashi@outlook.fr>
2024-09-17 00:00:27 +0200

    Refacto the app. better use of redis, Add logs, DTOs and response tooling

--- go.mod
+++ go.mod
@@ -3,7 +3,20 @@ module learningswarm
 go 1.22.5
 
 require (
+	github.com/go-playground/validator/v10 v10.22.1
+	github.com/google/uuid v1.6.0
+	github.com/redis/go-redis/v9 v9.6.1
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/redis/go-redis/v9 v9.6.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,14 +2,58 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
-	"strconv"
+	"runtime"
 	"time"
 
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
+
+type Hello struct {
+	ID   string
+	Name string
+}
+
+func (h Hello) MarshalBinary() ([]byte, error) {
+	return json.Marshal(h)
+}
+
+func (h *Hello) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, h)
+}
+
+type CreateHelloRequestDTO struct {
+	Name string `json:"name" validate:"required,min=3"`
+}
+
+func (h *CreateHelloRequestDTO) toHello() Hello {
+	return Hello{
+		Name: h.Name,
+	}
+}
+
+type HelloResponseDTO struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (h Hello) ToHelloResponseDTO() HelloResponseDTO {
+	return HelloResponseDTO{
+		ID:   h.ID,
+		Name: h.Name,
+	}
+}
+
+type ErrorResponseDTO struct {
+	Message string `json:"message"`
+}
 
 var (
 	rdb = redis.NewClient(&redis.Options{
@@ -17,14 +61,30 @@ var (
 		Password: "",                     // no password set
 		DB:       0,                      // use default DB
 	})
-	uid = 0
+	Validate = validator.New()
+)
+
+const (
+	HelloKey = "hello:"
+)
+
+var (
+	ErrCannotCreateHello        = errors.New("failed to create hello in db")
+	ErrCannotGetHello           = errors.New("failed to get hello from db")
+	ErrCannotUnmarshalHello     = errors.New("failed to unmarshal hello")
+	ErrCannotMarshalHello       = errors.New("failed to marshal hello")
+	ErrCannotMarshalHelloBinary = errors.New("failed to marshal hello binary")
+	ErrHelloNotFound            = errors.New("hello not found")
 )
 
 func main() {
 	setRedis()
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 	server := http.NewServeMux()
-	server.Handle("GET /hello/{id}", http.HandlerFunc(getHello))
-	server.Handle("POST /hello", http.HandlerFunc(setHello))
+	server.Handle("GET /hellos/{id}", http.HandlerFunc(getHelloByID))
+	server.Handle("GET /hellos", http.HandlerFunc(getHelloList))
+	server.Handle("POST /hellos", http.HandlerFunc(setHello))
+	slog.Info("Server start at :45000")
 	http.ListenAndServe(":45000", server)
 }
 
@@ -37,34 +97,141 @@ func setRedis() {
 	}
 }
 
-func getHello(w http.ResponseWriter, r *http.Request) {
+func shouldBindJSON(r *http.Request, v interface{}) error {
+	if err := json.NewDecoder(r.Body).Decode(v); err != nil {
+		return err
+	}
+	if err := Validate.Struct(v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getHelloByID(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
-	val, err := rdb.Get(r.Context(), id).Result()
+	val, err := rdb.Get(r.Context(), HelloKey+id).Result()
 	if err != nil {
-		w.Write([]byte(err.Error()))
-		w.Header().Add("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusInternalServerError)
+		if err == redis.Nil {
+			responseNotFound(w, errors.Join(ErrCannotGetHello, err, errors.New("id: "+id)))
+			return
+		}
+		responseInternalServerError(w, err)
 		return
 	}
-	w.Write([]byte("Hello " + val + " !"))
-	w.Header().Add("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
+	var res Hello
+	err = json.Unmarshal([]byte(val), &res)
+	if err != nil {
+		responseInternalServerError(w, err)
+		return
+	}
+	responseOK(w, res.ToHelloResponseDTO())
+}
+
+func getHelloList(w http.ResponseWriter, r *http.Request) {
+	keys, err := rdb.Keys(r.Context(), HelloKey+"*").Result()
+	if err != nil {
+		if err == redis.Nil {
+			responseNoContent(w)
+			return
+		}
+		responseInternalServerError(w, err)
+		return
+	}
+	var hellos []Hello
+	for _, id := range keys {
+		val, err := rdb.Get(r.Context(), id).Result()
+		if err != nil {
+			responseInternalServerError(w, errors.Join(ErrHelloNotFound, errors.New("id: "+id)))
+			return
+		}
+		var hello Hello
+		err = json.Unmarshal([]byte(val), &hello)
+		if err != nil {
+			responseInternalServerError(w, err)
+			return
+		}
+		hellos = append(hellos, hello)
+	}
+	var helloResponseList []HelloResponseDTO
+	for _, hello := range hellos {
+		helloResponseList = append(helloResponseList, hello.ToHelloResponseDTO())
+	}
+	responseOK(w, helloResponseList)
 }
 
 func setHello(w http.ResponseWriter, r *http.Request) {
-	storeHello(r)
-	w.Write([]byte("registered !"))
-	w.Header().Add("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
+	var createHelloRequestDTO CreateHelloRequestDTO
+	if err := shouldBindJSON(r, &createHelloRequestDTO); err != nil {
+		responseBadRequest(w, err)
+		return
+	}
+	hello := createHelloRequestDTO.toHello()
+	hello.ID = uuid.NewString()
+	newHello, err := storeCreateHello(r.Context(), hello)
+	if err != nil {
+		responseInternalServerError(w, err)
+		return
+	}
+	responseOK(w, newHello.ToHelloResponseDTO())
 }
 
-func storeHello(r *http.Request) {
-	b := make([]byte, r.ContentLength)
-	r.Body.Read(b)
-	fmt.Println("Request Body: ", string(b))
-	err := rdb.Set(r.Context(), strconv.Itoa(uid), string(b), 0).Err()
+func storeCreateHello(ctx context.Context, hello Hello) (Hello, error) {
+	helloBin, err := hello.MarshalBinary()
 	if err != nil {
-		panic(err)
+		return hello, errors.Join(ErrCannotMarshalHelloBinary, err)
 	}
-	uid++
+	err = rdb.Set(ctx, HelloKey+hello.ID, helloBin, 0).Err()
+	if err != nil {
+		return hello, errors.Join(ErrCannotCreateHello, err)
+	}
+	rs, err := rdb.Get(ctx, HelloKey+hello.ID).Result()
+	if err != nil {
+		return hello, errors.Join(ErrCannotGetHello, err)
+	}
+	var newHello Hello
+	err = json.Unmarshal([]byte(rs), &newHello)
+	if err != nil {
+		return newHello, errors.Join(ErrCannotUnmarshalHello, err)
+	}
+	return newHello, nil
+}
+
+func responseOK(w http.ResponseWriter, i interface{}) {
+	responseJSON, err := json.Marshal(i)
+	if err != nil {
+		responseInternalServerError(w, err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(responseJSON)
+}
+
+func responseErr(w http.ResponseWriter, statusCode int, err error) {
+	_, file, line, _ := runtime.Caller(2)
+	slog.Error(fmt.Sprint("file: ", file, ":", line, ": ", err.Error()))
+	var e ErrorResponseDTO
+	if err != nil {
+		e.Message = err.Error()
+	}
+	resp, _ := json.Marshal(e)
+	w.WriteHeader(statusCode)
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(resp)
+}
+
+func responseBadRequest(w http.ResponseWriter, err error) {
+	responseErr(w, http.StatusBadRequest, err)
+}
+
+func responseNoContent(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func responseNotFound(w http.ResponseWriter, err error) {
+	responseErr(w, http.StatusNotFound, err)
+}
+
+func responseInternalServerError(w http.ResponseWriter, err error) {
+	responseErr(w, http.StatusInternalServerError, err)
 }
